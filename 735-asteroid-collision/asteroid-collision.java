501a4058eebcf7ea2b1877e3aa44e228a599e4d1
Mansi <130751921+Mansi87@users.noreply.github.com>
2025-04-30 07:39:56 +0530

    Time: 5 ms (86.39%) | Memory: 45.8 MB (6.06%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+        List<Integer> st = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>0){
+                st.add(arr[i]);
+            }  
+            else{
+                while(!st.isEmpty() && st.get(st.size()-1)>0 && st.get(st.size()-1)<Math.abs(arr[i])){
+                   st.remove(st.size()-1);
+                }
+                if(!st.isEmpty() && st.get(st.size()-1)==Math.abs(arr[i])){
+                    st.remove(st.size()-1);
+                } 
+                else if(st.isEmpty() || st.get(st.size()-1)<0){
+                    st.add(arr[i]);
+                }
+            }
+        }
+
+        int[] res = new int[st.size()];
+        for(int i=0; i<st.size(); i++){
+            res[i] = st.get(i);
+        }
+        return res;
+    }
+}
