b91ad64239af3ce78ea8d3a2a4a096b730f4954e
Mansi <130751921+Mansi87@users.noreply.github.com>
2025-08-03 09:31:15 +0530

    Time: 7 ms (44.60%) | Memory: 46 MB (15.84%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int n = numCourses;
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+        int m = prerequisites.length;
+        for(int i=0; i<m; i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int indegree[] = new int[numCourses];
+        for(int i=0; i<n; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            topo.add(node);
+
+            for(int it: adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+
+        if(topo.size() == n) return true;
+        return false;
+    }
+}
